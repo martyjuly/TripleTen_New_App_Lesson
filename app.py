bbf747dfc8123af02eb6b6b9f05e43b779e969c1
martyjuly <51001375+martyjuly@users.noreply.github.com>
2025-03-09 18:16:03 -0500

    initial commit

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import scipy.stats
 import streamlit as st
 import time
 
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experiment of {number_of_trials} trials.')
+
+st.write('It is not a functional application yet. Under construction.')
+
 st.header('Tossing a Coin')
 
 chart = st.line_chart([0.5])
